ef9e4cf3e71e6a7631525f948f63ccb4371fda2c
schneekluth <5740913+schneekluth@users.noreply.github.com>
2022-04-27 20:58:28 +0200

    Update 0_windows.py

--- macros/0_windows.py
+++ macros/0_windows.py
@@ -13,18 +13,18 @@ app = {
         (0x000020, 'Mute', [[ConsumerControlCode.MUTE]]),
         (0x000020, 'Vol+', [[ConsumerControlCode.VOLUME_INCREMENT]]),
         # 2nd row ----------
-        (0x404000, 'TSKMGR', [Keycode.CONTROL, Keycode.SHIFT, Keycode.ESCAPE]),
-        (0x02180B, 'DSKTP', [Keycode.WINDOWS, 'd']),
-        (0x200000, 'CBHist', [Keycode.WINDOWS, 'v']),
+        (0x663300, 'TSKMGR', [Keycode.CONTROL, Keycode.SHIFT, Keycode.ESCAPE]),
+        (0x663300, 'DSKTP', [Keycode.WINDOWS, 'd']),
+        (0x663300, 'EXPL', [Keycode.WINDOWS, 'e']),
         # 3rd row ----------
-        (0x380e0e, 'Cut', [Keycode.CONTROL, 'x']),
-        (0x02180B, 'FCKSKP', [Keycode.WINDOWS, Keycode.ESCAPE]),
-        (0x663300, 'Lock', [Keycode.WINDOWS, 'l']),
-        # 4th row ----------
         (0x380e0e, 'SelAll', [Keycode.CONTROL, 'a']),
+        (0x02180B, 'CBHist', [Keycode.WINDOWS, 'v']),
+        (0x663300, 'SNIPPT', [Keycode.WINDOWS, 'r', 0.5, '%windir%\system32\SnippingTool.exe', Keycode.RETURN]),
+        # 4th row ----------
+        (0x380e0e, 'Cut', [Keycode.CONTROL, 'x']),
         (0x380e0e, 'Copy', [Keycode.CONTROL, 'c']),
         (0x380e0e, 'Paste', [Keycode.CONTROL, 'v']),
         # Encoder button ---
-        (0x000000, 'Quit', [Keycode.ALT, Keycode.F4])
+        (0x000000, 'NoSkype', [Keycode.WINDOWS, Keycode.ESCAPE])
     ]
 }
