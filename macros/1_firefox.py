55d9dc4067f698d6128b94706be8f8c36bd172e3
schneekluth <5740913+schneekluth@users.noreply.github.com>
2022-04-27 20:58:53 +0200

    Update 1_firefox.py

--- macros/1_firefox.py
+++ macros/1_firefox.py
@@ -8,21 +8,21 @@ app = {
     'macros' : [
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x000020, '+ TAB', [Keycode.CONTROL, 't']),
-        (0x200000, '+ WDW', [Keycode.CONTROL, 'n']),
+        (0x02180B, '+ TAB', [Keycode.CONTROL, 't']),
+        (0x380e0e, '+ WDW', [Keycode.CONTROL, 'n']),
         (0x000020, 'BW PW', [Keycode.CONTROL, Keycode.SHIFT, 'l']),
         # 2nd row ----------
-        (0x770de0, '- TAB', [Keycode.CONTROL, 'w']),
-        (0x770de0, '- WDW', [Keycode.CONTROL, Keycode.SHIFT, 'w']),
-        (0x000000, 'BW 2F', [Keycode.CONTROL, Keycode.SHIFT, 'l', 1.0, -Keycode.SHIFT, 'v', Keycode.ENTER]),
+        (0x02180B, '- TAB', [Keycode.CONTROL, 'w']),
+        (0x380e0e, '- WDW', [Keycode.CONTROL, Keycode.SHIFT, 'w']),
+        (0x000020, 'BW 2F', [Keycode.CONTROL, Keycode.SHIFT, 'l', 1.0, -Keycode.SHIFT, 'v', Keycode.ENTER]),
         # 3rd row ----------
-        (0x0de05e, '< Back', [Keycode.ALT, Keycode.LEFT_ARROW]),
-        (0x000000, 'Forw >', [Keycode.ALT, Keycode.RIGHT_ARROW]),
-        (0x000000, 'Reload', [Keycode.F5]),
+        (0x404000, '< Back', [Keycode.ALT, Keycode.LEFT_ARROW]),
+        (0x404000, 'Reload', [Keycode.F5]),
+        (0x404000, 'Forw >', [Keycode.ALT, Keycode.RIGHT_ARROW]),
         # 4th row ----------
-        (0x0de05e, '< Tab', [Keycode.CONTROL, Keycode.PAGE_UP]),
-        (0x0de05e, 'Tab >', [Keycode.CONTROL, Keycode.PAGE_DOWN]),
-        (0x0de05e, 'DL', [Keycode.CONTROL, 'j']),
+        (0x121212, '< Tab', [Keycode.CONTROL, Keycode.PAGE_UP]),
+        (0x121212, 'Tab >', [Keycode.CONTROL, Keycode.PAGE_DOWN]),
+        (0x002000, 'DL', [Keycode.CONTROL, 'j']),
         # Encoder button ---
         (0x000000, 'Quit', [Keycode.ALT, Keycode.F4])
     ]
